415ee5623528fa9062308dbc94f4977a2505c287
miseriae <zuyev.frm@gmail.com>
2020-03-23 16:42:48 +0200

    Add user registration and item ordering

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
 
     'core',
 ]
@@ -105,3 +106,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL ='/'
+
+#CRISPY FORMS 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
